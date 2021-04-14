2dec30bdbb2889ece5e32c4229785388626ebd5e
LuisMiguelCardenas <luismiguelcardenas2011@gmail.com>
2021-04-13 19:19:12 -0500

    Primer commit navbar

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = () => {
+    return ( 
+        <div className = "row col-3 pt-2" >
+            <img className =" col p-0" src="http://www.panditasco.com/images/logop.png"/>
+            <h2 className ="col">PanditasCo</h2>
+        </div>
+        
+     );
+}
+ 
+export default Logo;
